c7e4e5c283c2b19f11355bf1db791fa91c89a00d
fobosby <marduk@inbox.ru>
2013-10-10 23:41:23 +0300

    BaseTest

---
+++ SandBox/src/test/java/tests/BaseTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.sun.javafx.collections.transformation.SortableList;
+import com.sun.javafx.collections.transformation.SortedList;
+import org.testng.annotations.Test;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexey
+ * Date: 10/10/13
+ * Time: 11:35 PM
+ */
+public class BaseTest {
+
+    @Test
+    public void likeTest() {
+        SortedSet<String> sortedSet = new TreeSet<String>();
+        sortedSet.add("a");
+        sortedSet.add("b");
+        sortedSet.add("a");
+        System.out.print(sortedSet);
+
+    }
+
+}
